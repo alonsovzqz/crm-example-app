b38763999d191d32b67e10a8e7f0c81e48394295
Alonso Vazquez <haloanderson45@gmail.com>
2024-09-01 23:50:29 -0600

    Initial commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,11 +6,22 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "eslint-config-prettier"
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": "warn",
+    "no-unused-vars": [
+      "warning",
+      {
+        "vars": "all",
+        "args": "after-used",
+        "ignoreRestSiblings": true,
+        "argsIgnorePattern": "^_"
+      }
+    ],
+    "react/react-in-jsx-scope": "off"
   },
 };
